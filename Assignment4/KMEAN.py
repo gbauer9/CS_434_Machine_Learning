cda459b9dd7f75820450259a83a2d53d45df4500
James May <jimwmay2@gmail.com>
2018-05-27 13:58:29 -0700

    final kmean fix

--- Assignment4/KMEAN.py
+++ Assignment4/KMEAN.py
@@ -31,55 +31,46 @@ def dist(train_point, test_point):              #calculates distance between 2 d
         sum += pow(train_point[i] - test_point[i],2)
     return math.sqrt(sum)                       #returns int
 
-def accuracy(training, testing, k, i = None):           #calculates accuracy of method, returns float
-    testing_accuracy = 0
-    if i is None:                                       #for not using leave one out cross validation
-        for t in testing:
-            if t[0] == nearest_k(training, t, k):
-                testing_accuracy += 1
-        return testing_accuracy                         #returns float
-    else:                                               #for not using leave one out cross validation
-        for t in testing:
-            if t[0] == nearest_k(training, t, k, i):
-                testing_accuracy += 1
-        return testing_accuracy                         #returns float
-
-def k_mean(training, clusters):
+def k_mean(training, clusters):     #makes clusters with list of center points
     for t in training:
-        i = 0
         d = 0
-        min = 10000
-        for c in clusters:
-            if not (t==c[0]).all():
-                i += 1
-        if i >= len(clusters):
-            for r in range(len(clusters)):
-                d1 = dist(t, clusters[r][0])
-                if min > d1:
-                    min = d1
-                    d = r
-            clusters[d].append(t)
+        min = 1000000
+        for r in range(len(clusters)):
+            d1 = dist(t, clusters[r][0])
+            if min > d1:
+                min = d1
+                d = r
+        clusters[d].append(t)
     return clusters
 
-def nearest(cluster, point):
+def nearest(cluster, point):        #calculates the nearest point in a cluster
     min = dist(cluster[0], point)
     temp = cluster[0]
     for c in cluster:
-        d1 = dist(c, point)
-        if d1 < min:
+        d = dist(c, point)
+        if d < min:
             temp = c
-            min = d1
+            min = d
     return temp
 
-def centroid(cluster):
-    temp = []
+def SSE(cluster, center):           #calculates SSE for a cluster
     sum = 0
+    for c in cluster:
+        e = pow(dist(c, center),2)
+        sum += e
+    sum = sum/len(cluster)
+    return sum
+
+def centroid(cluster):              #calculates center of gravity and SSE or that cluster
+    temp = []
     for i in range(len(cluster[0])):
+        sum = 0
         for c in cluster:
             sum += c[i]
         temp.append(sum/len(cluster))
     np.array(temp)
-    temp = nearest(cluster, temp)
+    temp0 = SSE(cluster, temp)
+    temp = list([temp,temp0])
     return temp
 
 def add_c(cluster, centers):
@@ -97,22 +88,22 @@ def check(cluster, centers):
 train = "data-1.txt"
 start_time = time.time()
 training = norm_ary(train)
-k = 2
-x_axis = []
-y_axis = []
-while k <= 15:
+k = 2                       #sets initial k value
+x_axis = []                 # for plotting k-values
+y_axis = []                 # for plotting minimum SSE values
+while k <= 10:              #sets final k value
     print("starting k = ",k)
-    counter1 = 10
+    counter1 = 10           #sets how many times each k value runs
     minimum = []
-    while counter1 > 0:
+    y1_axis = []            # for plotting SSE values as it converges
+    while counter1 > 0:     #runs until counter drops to 0
         clusters = []
         temp1 = []
         temp1.append(training[0])
         rando = list(random.sample(range(1,len(training)), k-1))
-        print('random numbers: ', rando)
         clusters.append(temp1)
         i = 2
-        for r in rando:
+        for r in rando:     #random numbers seeding initial clusters
             temp = []
             temp.append(training[r])
             clusters.append(temp)
@@ -120,24 +111,31 @@ while k <= 15:
         centers = list(clusters)
         temp2 = 0
         counter = 0
-        while 1:
+        while 1:                                            #runs until break statement
             clusters = list(k_mean(training, clusters))
-            if abs(len(clusters[0]) - temp2) < .00001:
+            if abs(len(clusters[0]) - temp2) < .00001:      #break statement: when clusters stop changing size
                 break
             temp2 = len(clusters[0])
             temp_clusters = []
+            temp01 = []
             for r in range(k):
                 temp0 = []
-                temp0.append(centroid(clusters[r]))
+                temp_cent = list(centroid(clusters[r]))
+                temp0.append(temp_cent[0])
+                temp01.append(temp_cent[1])
                 temp_clusters.append(temp0)
             clusters = list(temp_clusters)
+            y1_axis.append(sum(temp01))
             counter += 1
+        #x1_axis = list(range(1,len(y1_axis)+1))
+        #plt.plot(x1_axis, y1_axis)  #plots             #plots SSE values as a function of iterations
+        #plt.show()
         counter1 -=1
-        minimum.append(counter)
-    print(minimum,"min: ", min(minimum))
+        minimum.append(temp01[len(temp01)-1])
+    #print(minimum,"min: ", min(minimum))
     y_axis.append(min(minimum, key=float))
     x_axis.append(k)
     k += 1
 print("--- %s seconds ---" % (time.time() - start_time))
-plt.plot(x_axis, y_axis)  #plots training, testing, and LOOCV errors of various k values
+plt.plot(x_axis, y_axis)                                #plots SSE values as a function of k-values
 plt.show()
